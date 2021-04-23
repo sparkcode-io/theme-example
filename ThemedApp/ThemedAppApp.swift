acd3acf268ac1b439d74d6eb5b373a5a9613da6c
AJ <aj@sparkcode.io>
2021-04-23 10:53:59 -0400

    Initial Commit

---
+++ ThemedApp/ThemedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemedAppApp.swift
+//  ThemedApp
+//
+//  Created by Sparkcode on 4/23/21.
+//
+
+import SwiftUI
+
+@main
+struct ThemedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
